d4b43c1b5ff3fa0c465dcc4a6ab5bfb97a6e3606
Caleb Maclennan <caleb@alerque.com>
2011-12-21 11:45:30 +0000

    add ititial version of widget from Subversion project
    Added some missing files from previous commit and tests of new widget.
    
    svn path=/sites/incil.info/devel/; revision=1768

---
+++ VerseSelect.js
@@ -0,0 +1,9 @@
+define([
+	"dojo",
+	"dijit",
+	"dojo/_base/declare",
+	"dijit/form/FilteringSelect"
+], function(dojo, dijit){
+	return dojo.declare("incilinfo.VerseSelect", [dijit.form.FilteringSelect], {
+	}); 
+});
