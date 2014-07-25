a681de9db7a743b3319dc080560062362f7baec8
Caleb Maclennan <caleb@alerque.com>
2014-07-25 14:03:36 +0300

    straighten out namespace and event triggers between child widgets

--- VerseSelect.js
+++ VerseSelect.js
@@ -12,13 +12,16 @@ define([
     "incilinfo.RakamSpinner",
     [dijit.form.NumberSpinner],
     {
+
       selectOnClick: true,
+
       newMax: function(val) {
         this.constraints.max = val;
         if (val < this.get('value')) {
           this.set('value', val);
         }
       },
+
       // Override scroll function to force increment
       // http://stackoverflow.com/q/24489968/313192
       adjust: function(val, delta) {
@@ -27,6 +30,7 @@ define([
         this.onChange();
         return ret;
       },
+
       postCreate: function() {
         this.inherited(arguments);
       }
@@ -36,17 +40,19 @@ define([
     "incilinfo.VerseSelect",
     [dijit._Widget, dijit._TemplatedMixin],
     {
+
       widgetsInTemplate: true,
 
-      templateString: '<div class="dijitInline" id="${id}" data-dojo-attach-point="wrapper"></div>',
+      templateString: '<div class="dijitInline" id="${id}" ' +
+        'data-dojo-attach-point="wrapper"></div>',
 
       postCreate: function() {
-        var store = new dojo.data.ItemFileReadStore({
+        this.store = new dojo.data.ItemFileReadStore({
           url: this.storeUrl
         });
-        var kitap = new dijit.form.FilteringSelect({
+        this.kitap = new dijit.form.FilteringSelect({
           placeHolder: "Kitap",
-          store: store,
+          store: this.store,
           style: 'width: 12em',
           searchAttr: "name",
           autocomplete: false,
@@ -55,38 +61,48 @@ define([
           queryExpr: '*${0}*',
           searchDelay: 500
         });
-        var bolum = new incilinfo.RakamSpinner({
+        this.bolum = new incilinfo.RakamSpinner({
           placeHolder: "Bölüm",
           constraints: {min:1, max:1},
           style: 'width: 4em;'
         });
-        var ayet = new incilinfo.RakamSpinner({
+        this.ayet = new incilinfo.RakamSpinner({
           placeHolder: "Ayet",
           constraints: {min:1, max:1},
           style: 'width: 4em;'
         });
-        dojo.connect(kitap, 'onChange', function() {
-            // Set chapter spinner max value to number of chapters in book
-            bolum.newMax(kitap.item.chapters);
-            // Rewind to chapter one on book change
-            bolum.set('value', 1);
-            bolum.onChange();
-          });
-        dojo.connect(bolum, 'onChange', function() {
-            // Set verse spinner max value to number of verses in chapter
-            ayet.newMax(kitap.item.ayetler[this.value]);
-            // Rewind to verse one on chapter change
-            ayet.set('value', 1);
-            ayet.onChange();
-          });
-        dojo.connect(ayet, 'onChange', function() {
-          });
-        kitap.placeAt(this.wrapper);
-        bolum.placeAt(this.wrapper);
-        ayet.placeAt(this.wrapper);
-        this.kitap = kitap;
-        this.bolum = bolum;
-        this.ayet = ayet;
+        dojo.connect(this.kitap, 'onChange', this, 'changeKitap');
+        dojo.connect(this.bolum, 'onChange', this, 'changeBolum');
+        dojo.connect(this.ayet, 'onChange', this, 'changeAyet');
+        this.kitap.placeAt(this.wrapper);
+        this.bolum.placeAt(this.wrapper);
+        this.ayet.placeAt(this.wrapper);
+      },
+
+      changeKitap: function() {
+        // Set chapter spinner max value to number of chapters in book
+        this.bolum.newMax(this.kitap.item.chapters);
+        // Rewind to chapter one on book change
+        this.bolum.set('value', 1);
+        //this.bolum.onChange();
+      },
+
+      changeBolum: function() {
+        // This won't work until after the book selector has been set
+        if (this.kitap.item == null) return;
+        // Set verse spinner max value to number of verses in chapter
+        this.ayet.newMax(this.kitap.item.ayetler[this.bolum.get('value')]);
+        // Rewind to verse one on chapter change
+        this.ayet.set('value', 1);
+        //this.ayet.onChange();
+      },
+
+      changeAyet: function() {
+        console.log(
+          this.kitap.get('value'),
+          this.bolum.get('value'),
+          this.ayet.get('value')
+        );
       },
 
       setLocation: function(val) {
