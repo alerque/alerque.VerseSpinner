cd81b6ab4b17d1fcacee51439da2d962b0e1b28e
Caleb Maclennan <caleb@alerque.com>
2015-04-16 17:34:18 +0300

    translate all variables and funtions to English for general release

--- VerseSpinner.js
+++ VerseSpinner.js
@@ -48,9 +48,9 @@ define([
     {
 
       widgetsInTemplate: true,
-      _hedef: false,
-      _hedefBolum: 1,
-      _hedefAyet: 1,
+      _target: false,
+      _targetChapter: 1,
+      _targetVerse: 1,
 
       templateString: '<div class="dijitInline" id="${id}" ' +
         'data-dojo-attach-point="wrapper"></div>',
@@ -59,8 +59,8 @@ define([
         this.store = new ItemFileReadStore({
           url: this.storeUrl
         });
-        this.kitap = new FilteringSelect({
-          placeHolder: "Kitap",
+        this.book = new FilteringSelect({
+          placeHolder: "Book",
           store: this.store,
           style: 'width: 12em',
           searchAttr: "name",
@@ -70,104 +70,104 @@ define([
           queryExpr: '*${0}*',
           searchDelay: 0
         });
-        this.bolum = new alerque.ReferenceNumberSpinner({
-          placeHolder: "Bölüm",
+        this.chapter = new alerque.ReferenceNumberSpinner({
+          placeHolder: "Chapter",
           constraints: {min:1, max:1},
           style: 'width: 4em;'
         });
-        this.ayet = new alerque.ReferenceNumberSpinner({
-          placeHolder: "Ayet",
+        this.verse = new alerque.ReferenceNumberSpinner({
+          placeHolder: "Verse",
           constraints: {min:1, max:1},
           style: 'width: 4em;'
         });
-        on(this.kitap, 'change', lang.hitch(this, this.kitapDegistir));
-        on(this.bolum, 'change', lang.hitch(this, this.bolumDegistir));
-        on(this.ayet, 'change', lang.hitch(this, this.ayetDegistir));
-        this.kitap.placeAt(this.wrapper);
-        this.bolum.placeAt(this.wrapper);
-        this.ayet.placeAt(this.wrapper);
+        on(this.book, 'change', lang.hitch(this, this.changeBook));
+        on(this.chapter, 'change', lang.hitch(this, this.changeChapter));
+        on(this.verse, 'change', lang.hitch(this, this.changeVerse));
+        this.book.placeAt(this.wrapper);
+        this.chapter.placeAt(this.wrapper);
+        this.verse.placeAt(this.wrapper);
       },
 
-      kitapDegistir: function() {
+      changeBook: function() {
         // Set chapter spinner max value to number of chapters in book
-        this.bolum.newMax(this.kitap.item.chapters);
-        var prev = this.bolum.get('value');
+        this.chapter.newMax(this.book.item.chapters);
+        var prev = this.chapter.get('value');
         // Rewind to chapter one on book change
-        this.bolum.set('value', this._hedefBolum);
+        this.chapter.set('value', this._targetChapter);
         // Trigger a chapter reload on book change even if the chapter number
         // didn't change (as in moving from chapter 1 » 1 of different books)
-        if (prev == this._hedefBolum) {
-          this.bolum.onChange();
+        if (prev == this._targetChapter) {
+          this.chapter.onChange();
         }
       },
 
-      bolumDegistir: function() {
+      changeChapter: function() {
         // This won't work until after the book selector has been set
-        if (this.kitap.item === null) {
+        if (this.book.item === null) {
           return;
         }
         // Set verse spinner max value to number of verses in chapter
-        this.ayet.newMax(this.kitap.item.ayetler[this.bolum.get('value')]);
-        var prev = this.ayet.get('value');
+        this.verse.newMax(this.book.item.verseler[this.chapter.get('value')]);
+        var prev = this.verse.get('value');
         // Rewind to verse one on chapter change
-        this.ayet.set('value', this._hedefAyet);
+        this.verse.set('value', this._targetVerse);
         // Trigger a verse reload on chapter change even if the chapter number
         // didn't change (as in moving from verse 1 » 1 of different chapters)
-        if (prev == this._hedefAyet) {
-          this.ayet.onChange();
+        if (prev == this._targetVerse) {
+          this.verse.onChange();
         }
-        this.referansaSeyret();
+        this.navigateToReference();
       },
 
-      ayetDegistir: function() {
+      changeVerse: function() {
         console.log("scoll to verse");
       },
 
-      referansaSeyret: function() {
+      navigateToReference: function() {
         // Don't actually navigate if we were given a spinner target value
-        if (this._hedef) {
-          this._hedefBolum = 1;
-          this._hedefAyet = 1;
-          this._hedef = false;
+        if (this._target) {
+          this._targetChapter = 1;
+          this._targetVerse = 1;
+          this._target = false;
           return;
         }
-        var kitap = this.kitap.get('value');
-        var kitapAdi = this.kitap.get('displayedValue');
-        var bolum = this.bolum.get('value');
-        var ayet = this.ayet.get('value');
+        var book = this.book.get('value');
+        var book_name = this.book.get('displayedValue');
+        var chapter = this.chapter.get('value');
+        var verse = this.verse.get('value');
         var node = domConstruct.create("a", {
-            href: "/kitap/" + kitap + "/" + bolum,
+            href: "/book/" + book + "/" + chapter,
             onclick: "return incilsayfa(this);",
-            title: kitapAdi + " " + bolum
+            title: book_name + " " + chapter
           });
-        this._referansaSeyret(node);
+        this._navigateToReference(node);
       },
 
       // This is so mouse scrolls etc don't trigger rapid fire network requests
-      _referansaSeyret: debounce(function(node) {
+      _navigateToReference: debounce(function(node) {
         incilsayfa(node);
       }, 200),
 
       // Set the spinner to a current scroll location (without triggering
       // navigation)
-      referansiBelirle: function(val) {
-        this._hedefBolum = val.bolum;
-        this._hedefAyet = val.ayet;
-        if (this.kitap.get('value') != val.kitap) {
-          this._hedef = true;
-          this.kitap.set('value', val.kitap);
+      setReference: function(val) {
+        this._targetChapter = val.chapter;
+        this._targetVerse = val.verse;
+        if (this.book.get('value') != val.book) {
+          this._target = true;
+          this.book.set('value', val.book);
         } else {
-          if (this.bolum.get('value') != val.bolum) {
-            this._hedef = true;
-            this.bolum.set('value', val.bolum);
+          if (this.chapter.get('value') != val.chapter) {
+            this._target = true;
+            this.chapter.set('value', val.chapter);
           } else {
-            if (this.ayet.get('value') != val.ayet) {
-              this._hedef = true;
-              this.ayet.set('value', val.ayet);
+            if (this.verse.get('value') != val.verse) {
+              this._target = true;
+              this.verse.set('value', val.verse);
             } else {
-              this._hedef = false;
-              this._hedefBolum = 1;
-              this._hedefAyet = 1;
+              this._target = false;
+              this._targetChapter = 1;
+              this._targetVerse = 1;
             }
           }
         }
